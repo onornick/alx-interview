4df82f478fad5d73a6c779382b31fdc67da83f4d
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-04 15:32:11 +0300

    changed the implementation to reduce complexity

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,7 @@
 def pascal_triangle(n):
-    triangle = []
-    for row_num in range(n):
-        # Create a new row
-        row = [1]  # First element of every row is always 1
-        # Fill the row
-        for j in range(1, row_num):
-            row.append(triangle[row_num - 1][j - 1] + triangle[row_num - 1][j])
-        if row_num > 0:
-            row.append(1)  # Last element of every row is always 1
+    triangle = [[1]]
+    for row_num in range(1, n):
+        # Generate the next row using list comprehension
+        row = [1] + [triangle[row_num - 1][j] + triangle[row_num - 1][j + 1] for j in range(row_num - 1)] + [1]
         triangle.append(row)
     return triangle
