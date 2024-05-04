7b3f1f169cc33349033a2663a627fc9d820087d3
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-04 15:43:19 +0300

    changed the implementation of pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,24 @@
+#!/usr/bin/python3
+"""pascal's triangle for any number"""
+
+
 def pascal_triangle(n):
-    triangle = [[1]]
-    for _ in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)] + [1]
-        triangle.append(new_row)
+    """
+    returns a list of lists
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
     return triangle
