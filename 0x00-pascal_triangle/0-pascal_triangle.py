54ba3e354d3498f24e6a84ae12bf25f63d67ae9c
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-05-04 15:38:40 +0300

    changed the implementation to use list comprehensions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,7 @@
 def pascal_triangle(n):
     triangle = [[1]]
-    for row_num in range(1, n):
-        # Generate the next row using list comprehension
-        row = [1] + [triangle[row_num - 1][j] + triangle[row_num - 1][j + 1] for j in range(row_num - 1)] + [1]
-        triangle.append(row)
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)] + [1]
+        triangle.append(new_row)
     return triangle
